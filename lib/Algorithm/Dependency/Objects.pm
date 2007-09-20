78830f31bfd3800f7571ae9fb87fcd00eeac4fbe
Yuval Kogman <nothingmuch@woobling.org>
2007-09-20 20:39:14 +0000

    versions

--- lib/Algorithm/Dependency/Objects.pm
+++ lib/Algorithm/Dependency/Objects.pm
@@ -5,7 +5,7 @@ package Algorithm::Dependency::Objects;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Scalar::Util qw/blessed/;
 use Carp qw/croak/;
--- lib/Algorithm/Dependency/Objects/Ordered.pm
+++ lib/Algorithm/Dependency/Objects/Ordered.pm
@@ -6,8 +6,6 @@ use base qw/Algorithm::Dependency::Objects/;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
-
 use Scalar::Util qw/refaddr/;
 use Carp qw/croak/;
 
